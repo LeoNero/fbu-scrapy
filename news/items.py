e25697c69f0265669e573a77efc8539585267261
LeoNero <leonardofelipenerone@gmail.com>
2019-08-02 11:11:52 -0700

    items: add necessary fields

--- news/items.py
+++ news/items.py
@@ -1,4 +1,8 @@
 import scrapy
 
 class NewsItem(scrapy.Item):
-    pass
+    name = scrapy.Field()
+    body_snippet = scrapy.Field()
+    body = scrapy.Field()
+    author = scrapy.Field()
+    source = scrapy.Field()
