e8bb253a3c0f85ef9710ffff2afd13f7e6b48a04
LeoNero <leonardofelipenerone@gmail.com>
2019-08-02 16:24:16 -0700

    spiders: add CNN spider

---
+++ news/spiders/cnn_spider.py
@@ -0,0 +1,39 @@
+import scrapy
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
+from news.items import NewsItem
+
+class CnnSpider(CrawlSpider):
+    name = "cnn"
+    allowed_domains = ["www.cnn.com"]
+    start_urls = [
+        "https://www.cnn.com/politics"
+    ]
+
+    rules = (
+        Rule(LinkExtractor(), callback='parse_item', follow=True),
+    )
+
+    def parse_item(self, response):
+        splitted_url = response.url.split('/')
+
+        if (len(splitted_url) < 4):
+            return
+
+        if (splitted_url[3] != '2019'):
+            return
+
+        news = ItemLoader(item=NewsItem(), response=response)
+        news.add_css('name', 'h1.pg-headline::text')
+
+        if response.css('span.metadata__byline__author a::text').get() == None:
+            news.add_css('author', 'span.metadata__byline__author::text')
+        else:
+            news.add_css('author', 'span.metadata__byline__author a::text')
+
+        news.add_css('body_snippet', '.zn-body__paragraph.speakable *::text')
+        news.add_css('body', '.zn-body__paragraph *::text')
+        news.add_value('source', response.url)
+
+        return news.load_item()
